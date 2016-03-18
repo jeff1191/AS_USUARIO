f9cc8b011e1eda93a69187a6b8c895efe6aa8194
juanluar <jeanleon003@hotmail.com>
2016-03-18 13:49:42 +0100

    Clase que falta

--- as_usuario/src/main/java/es/ucm/as_usuario/presentacion/MainActivity.java
+++ as_usuario/src/main/java/es/ucm/as_usuario/presentacion/MainActivity.java
@@ -117,7 +117,7 @@ public class MainActivity extends Activity {
     public void ayuda(View v) {
         Controlador.getInstancia().ejecutaComando(ListaComandos.CARGAR_BBDD, null);
         //Envio datos para el correo
-        Intent correo = new Intent (getApplicationContext(), EnviarCorreo.class);
+        Intent correo = new Intent (getApplicationContext(), Correo.class);
         //correo.putExtra("destinatario", "juanluar@ucm.es");
         //correo.putExtra("titulo", "Primera prueba");
         //correo.putExtra("texto", "Bueno parece que esto ya marcha... o al menos una parte ;)");
